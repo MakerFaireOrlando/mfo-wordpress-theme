b7fb7eba26f4dc70a14bbc20f7d4667cf92ed20c
Ian Cole <ian.cole@gmail.com>
2017-06-22 12:05:09 +0000

    Adding comments.php per theme guidelines

---
+++ comments.php
@@ -0,0 +1,56 @@
+<?php
+/*
+ * derived from Coraline
+ *
+ */
+
+if ( post_password_required() )
+	return;
+?>
+
+<div id="comments">
+
+	<?php // You can start editing here -- including this comment! ?>
+
+	<?php if ( have_comments() ) : ?>
+		<h3 id="comments-title">
+			<?php
+				printf( _n( 'One response to &ldquo;%2$s&rdquo;', '%1$s responses to &ldquo;%2$s&rdquo;', get_comments_number(), 'coraline' ),
+					number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
+			?>
+		</h3>
+
+		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
+		<div class="navigation">
+			<div class="nav-previous"><?php previous_comments_link( __( '<span class="meta-nav">&larr;</span> Older Comments', 'coraline' ) ); ?></div>
+			<div class="nav-next"><?php next_comments_link( __( 'Newer Comments <span class="meta-nav">&rarr;</span>', 'coraline' ) ); ?></div>
+		</div> <!-- .navigation -->
+		<?php endif; // check for comment navigation ?>
+
+		<ol class="commentlist">
+			<?php wp_list_comments( array( 'callback' => 'coraline_comment' ) ); ?>
+		</ol>
+
+		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
+		<div class="navigation">
+			<div class="nav-previous"><?php previous_comments_link( __( '<span class="meta-nav">&larr;</span> Older Comments', 'coraline' ) ); ?></div>
+			<div class="nav-next"><?php next_comments_link( __( 'Newer Comments <span class="meta-nav">&rarr;</span>', 'coraline' ) ); ?></div>
+		</div><!-- .navigation -->
+		<?php endif; // check for comment navigation ?>
+
+	<?php else : // or, if we don't have comments:
+
+		/* If there are no comments and comments are closed,
+		 * let's leave a little note, shall we?
+		 * But only on posts! We don't really need the note on pages.
+		 */
+		if ( ! comments_open() && ! is_page() ) :
+		?>
+		<p class="nocomments"><?php _e( 'Comments are closed.', 'coraline' ); ?></p>
+		<?php endif; // end ! comments_open() ?>
+
+	<?php endif; // end have_comments() ?>
+
+	<?php comment_form(); ?>
+
+</div><!-- #comments -->
