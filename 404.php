40b6e97cc9e620a9edd0eb685fd54ab13de27801
chadisrad <chadselish@gmail.com>
2017-06-30 18:35:34 -0400

    Updated 404 with universal links

--- 404.php
+++ 404.php
@@ -12,9 +12,9 @@ get_header(); ?>
 		<div id="post-0" class="post error404 not-found">
 			<h1 class="entry-title"><?php _e( 'Page Not Found', 'coraline' ); ?></h1>
 			<div class="entry-content">
-				<p>Apologies, but the page you requested could not be found. It might not exist, or may require <a href="http://www.makerfaireorlando.com/makerlogin">login</a>.</p>
+				<p>Apologies, but the page you requested could not be found. It might not exist, or may require <a href="/makerlogin">login</a>.</p>
 
-				<p>If you are a Maker attempting to view your Maker Profile, or exhibits, etc. - please <a href="http://www.makerfaireorlando.com/makerlogin">login</a> first. </p>
+				<p>If you are a Maker attempting to view your Maker Profile, or exhibits, etc. - please <a href="/makerlogin">login</a> first. </p>
 
 				<p>Can't find something? Try searching...</p>
 
